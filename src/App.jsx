03a92c42e7b09caccab98898a5566b2ec708be7f
Alexis <alexcellentart@gmail.com>
2024-08-24 23:20:33 -0400

    Componentization Finished. Extension Goals reached.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@
 import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import Blue from "./components/Blue";
-import Red from "./components/Red";
-import Green from "./components/Green";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import MainContainer from "./components/MainContainer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,20 +11,9 @@ function App() {
   return (
     <>
       <h1>React Router Demonstration</h1>
-      <div id="navbar">
-      <Link to="/">Home</Link>
-        <Link to="/blue">Blue</Link>
-        <Link to="/red">Red</Link>
-        <Link to="/green">Green</Link>
-      </div>
-      <div id="main-section">
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="blue" element={<Blue/>} />
-          <Route path="red" element={<Red/>} />
-          <Route path="green" element={<Green/>} />
-        </Routes>
-      </div>
+      <Navbar></Navbar>
+      <MainContainer></MainContainer>
+      <footer><Navbar></Navbar></footer>
     </>
   );
 }
